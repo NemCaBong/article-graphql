b4eb6206579315914f04764c56a55db40562e9af
Nguyen Minh Hoang <nemcabong@gmail.com>
2023-12-16 04:54:20 +0700

    Sort theo dieu kien trong graphql

--- resolvers/article.resolver.ts
+++ resolvers/article.resolver.ts
@@ -3,10 +3,17 @@ import Category from "../models/category.model";
 
 export const resolversArticle = {
   Query: {
-    getListArticle: async () => {
+    getListArticle: async (_, args) => {
+      const { sortKey, sortValue } = args;
+
+      const sort = {};
+      if (sortKey && sortValue) {
+        sort[sortKey] = sortValue;
+      }
+
       const articles = await Article.find({
         deleted: false,
-      });
+      }).sort(sort);
 
       return articles;
     },
--- typeDefs/article.typeDefs.ts
+++ typeDefs/article.typeDefs.ts
@@ -16,7 +16,7 @@ export const typeDefsArticle = gql`
   }
 
   type Query {
-    getListArticle: [Article]
+    getListArticle(sortKey: String, sortValue: String): [Article]
     getArticle(id: ID): Article
   }
 
